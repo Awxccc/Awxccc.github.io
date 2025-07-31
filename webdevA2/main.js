388f51c1c61107d234a940f043e3516cd7373a29
Awxccc <sancturez@gmail.com>
2025-08-01 05:09:03 +0800

    New

--- webdevA2/main.js
+++ webdevA2/main.js
@@ -651,3 +651,33 @@ window.addEventListener("scroll", () => {
   btn.style.display = nearBottom ? "block" : "none";
 });
 
+const btnFS=document.querySelector("#btnFS");
+let fullscreenToggle = false;
+btnFS.addEventListener("click",enterFullscreen);
+function enterFullscreen() { //must be called by user generated event
+  if(fullscreenToggle == false)
+  {
+if (document.documentElement.requestFullscreen) {
+document.documentElement.requestFullscreen();
+} else if (document.documentElement.mozRequestFullScreen) { // Firefox
+document.documentElement.mozRequestFullScreen();
+} else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+document.documentElement.webkitRequestFullscreen();
+} else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+document.documentElement.msRequestFullscreen();
+}
+fullscreenToggle = true;
+  }
+else{
+if (document.exitFullscreen) {
+document.exitFullscreen();
+} else if (document.mozCancelFullScreen) { // Firefox
+document.mozCancelFullScreen();
+} else if (document.webkitExitFullscreen) { // Chrome, Safari, and Opera
+document.webkitExitFullscreen();
+} else if (document.msExitFullscreen) { // IE/Edge
+document.msExitFullscreen();
+}
+fullscreenToggle = false;
+}
+}
